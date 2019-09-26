546b975cbf018109cbc3c1b6a33b0dd65dce2eab
zeelmehta389 <zeelmehta1999@gmail.com>
2019-09-26 16:12:01 +0530

    partially done maps...

--- app/src/main/java/com/example/admin/she/MapsTab.java
+++ app/src/main/java/com/example/admin/she/MapsTab.java
@@ -3,16 +3,24 @@ package com.example.admin.she;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MapsTab extends Fragment{
+
+    GoogleMap googleMap;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.tab_maps, container , false);
         return view;
+
+
     }
 
 }
