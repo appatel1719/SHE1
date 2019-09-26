48594080215eb6c45c389c53c30f92247d3938c9
appatel1719 <appatel1719@gmail.com>
2019-09-26 13:17:50 +0530

    Commiting after renaming

--- app/src/main/java/com/example/admin/she/EmergencyTab.java
+++ app/src/main/java/com/example/admin/she/EmergencyTab.java
@@ -7,11 +7,11 @@ import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-public class TabOne extends Fragment{
+public class EmergencyTab extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.tab1 , container , false);
+        View view = inflater.inflate(R.layout.tab_emergency, container , false);
         return view;
 
     }
