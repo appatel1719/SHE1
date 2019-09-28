9f8d46bfe968996d90e4dd445e941b9419f49880
zeelmehta389 <zeelmehta1999@gmail.com>
2019-09-28 16:11:28 +0530

    Commiting after redesigning UI from Zeel

--- app/src/main/java/com/example/admin/she/Login.java
+++ app/src/main/java/com/example/admin/she/Login.java
@@ -3,13 +3,21 @@ package com.example.admin.she;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Login extends AppCompatActivity {
 
     private Button button;
     private Button button1;
+    EditText name,address,age,id,number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,11 @@ public class Login extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button7);
         button1 = (Button) findViewById(R.id.button6);
+        name = (EditText) findViewById(R.id.editText7);
+        address = (EditText) findViewById(R.id.editText8);
+        age = (EditText) findViewById(R.id.editText9);
+        id = (EditText) findViewById(R.id.editText5);
+        number = (EditText) findViewById(R.id.editText6);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,9 +47,29 @@ public class Login extends AppCompatActivity {
         });
     }
 
-    public void openlogin2(){
-        Intent intent = new Intent(this , login2.class);
-        startActivity(intent);
+    public void openlogin2() {
+        //validation of name, address, id , phone number
+        if (isEmpty(name)) {
+            name.setError("Enter your name!");
+        }
+        else if (isEmpty(address)) {
+            address.setError("Enter valid address!");
+        }
+        else if (isEmpty(age)) {
+            age.setError("Enter your age!");
+        }
+        else if (isEmail(id) == false) {
+                id.setError("Enter valid email!");
+        }
+        else if(isValidPhone(number)){
+                number.setError("Enter contact number");
+        }
+        else{
+                    Intent intent = new Intent(this, login2.class);
+                    startActivity(intent);
+        }
+
+
     }
 
     public void openlogin0(){
@@ -44,4 +77,18 @@ public class Login extends AppCompatActivity {
         startActivity(intent);
     }
 
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
+    }
+
+    boolean isEmail(EditText text) {
+        CharSequence email = text.getText().toString();
+        return (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean isValidPhone(EditText num) {
+        CharSequence phone = num.getText().toString();
+        return (!TextUtils.isEmpty(phone) && Patterns.PHONE.matcher(phone).matches());
+    }
 }
--- app/src/main/java/com/example/admin/she/login3.java
+++ app/src/main/java/com/example/admin/she/login3.java
@@ -3,13 +3,16 @@ package com.example.admin.she;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class login3 extends AppCompatActivity {
 
     private Button button;
     private Button button1;
+    EditText name , pass , confirmPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,9 @@ public class login3 extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button8);
         button1 = (Button) findViewById(R.id.button2);
+        name = (EditText) findViewById(R.id.editText2);
+        pass = (EditText) findViewById(R.id.editText4);
+        confirmPass = (EditText) findViewById(R.id.editText10);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +46,23 @@ public class login3 extends AppCompatActivity {
     }
 
     public void openlogin4(){
-        Intent intent = new Intent(this, login4.class);
-        startActivity(intent);
+        if (isEmpty(name)) {
+            name.setError("Enter your name!");
+        }
+        else if (isEmpty(pass)) {
+            name.setError("Enter valid password!");
+        }
+        else if (isEmpty(confirmPass)) {
+            name.setError("Enter your password again!");
+        }
+        else{
+            Intent intent = new Intent(this, login4.class);
+            startActivity(intent);
+        }
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence str = text.getText().toString();
+        return TextUtils.isEmpty(str);
     }
 }
